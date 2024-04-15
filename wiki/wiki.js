f414ce5bcee5b0489c3fe1b9bac655e7522cbd70
Marcos Alves <MarcosAlves25@proton.me>
2024-04-14 23:35:55 -0300

    Abertura do git

--- wiki/wiki.js
+++ wiki/wiki.js
@@ -1,34 +1,42 @@
 // ---------------------------------------------
 
 // Obtenha todas as tags
-var tags = document.querySelectorAll('.conteudo-wiki-search-tagcloud a');
+var tags = document.querySelectorAll(".conteudo-wiki-search-tagcloud a");
 
 // Adicione um evento de clique a cada tag
-tags.forEach(function(tag) {
-  tag.addEventListener('click', function(e) {
+tags.forEach(function (tag) {
+  tag.addEventListener("click", function (e) {
     e.preventDefault();
 
     // Obtenha o valor da tag
-    var tagValue = this.getAttribute('href').substring(1);
+    var tagValue = this.getAttribute("href").substring(1);
 
     // Obtenha todos os cards
-    var cards = document.querySelectorAll('.conteudo-wiki-cards-card');
+    var cards = document.querySelectorAll(".conteudo-wiki-cards-card");
 
     // Percorra todos os cards
-    cards.forEach(function(card) {
+    cards.forEach(function (card) {
       // Verifique se o card contém a tag
-      var cardTags = card.querySelectorAll('.conteudo-wiki-cards-card-tags a');
-      var hasTag = Array.from(cardTags).some(function(cardTag) {
-        return cardTag.getAttribute('href').substring(1) === tagValue;
+      var cardTags = card.querySelectorAll(".conteudo-wiki-cards-card-tags a");
+      var hasTag = Array.from(cardTags).some(function (cardTag) {
+        return cardTag.getAttribute("href").substring(1) === tagValue;
       });
 
       // Se o card contiver a tag, mostre-o. Caso contrário, esconda-o.
       if (hasTag) {
-        card.style.display = 'flex';
+        card.style.display = "flex";
       } else {
-        card.style.display = 'none';
+        card.style.display = "none";
       }
     });
+  });
+});
+
+// ---------------------------------------------
+
+document.querySelectorAll(".conteudo-wiki-cards-card").forEach((card) => {
+  card.addEventListener("click", function () {
+    window.location.href = this.getAttribute("data-url");
   });
 });
 
